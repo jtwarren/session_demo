ae57c5d23912a242b2c672b4c537d140cc4c1f5a
Jeffrey Warren <jtwarren@mit.edu>
2013-02-27 23:58:21 -0500

    Added decode to decode base64 cookies, use curl -I to look at headers and decode using file provided.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # before_filter :add_value_to_session
+
+  # private
+  # def add_value_to_session
+  #   session[:message] = "Hello World!"
+  # end
+
   private
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
---
+++ decode.rb
@@ -0,0 +1,2 @@
+require 'rack'
+puts Rack::Session::Cookie::Base64::Marshal.new.decode(ARGV[0])
