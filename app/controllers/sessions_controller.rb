1007ef07cf24fe59cd193bd970fba2b840543581
Jeffrey Warren <jtwarren@mit.edu>
2013-03-02 12:13:51 -0500

    Added comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-   user = User.find_by_email(params[:email])
-   if user && user.authenticate(params[:password])
-     session[:user_id] = user.id
-     redirect_to root_url, :notice => "Logged in"
-   else
-     flash.now.alert = "Invalid email or password"
-     render "new"
-   end
+    user = User.find_by_email(params[:email])
+
+    # Authenticate method provided by has_secure_password
+    # on the user class.
+    if user && user.authenticate(params[:password])
+      # Set the session for the logged in user.
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "Logged in"
+    else
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
   end
 
   def destroy
-   session[:user_id] = nil
-   redirect_to root_url, :notice=> "Logged out"
+    session[:user_id] = nil
+    redirect_to root_url, :notice=> "Logged out"
   end
 end
